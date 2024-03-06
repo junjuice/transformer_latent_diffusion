f19fd94386620b34a2ff773d9b09b16f518037c4
junjuice0 <junjuice0@gmail.com>
2024-03-06 11:46:13 +0900

    fix

--- train_tpu.py
+++ train_tpu.py
@@ -6,7 +6,7 @@ from lightning.pytorch.loggers import WandbLogger
 from tld.lightnings import DenoiserPL
 
 class Config:
-    def __init__(self, data: dict):
+    def __init__(self, data):
         for key in data.keys():
             setattr(self, key, data[key])
 
